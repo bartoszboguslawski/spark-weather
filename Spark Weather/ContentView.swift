10beb9966e45e2590ae6ca8480df86a6ee427061
Bartosz Bogusławski <Boguslawski56@gmail.com>
2022-05-16 19:56:45 +0200

    Initial Commit

---
+++ Spark Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spark Weather
+//
+//  Created by Bartosz Bogusławski on 16/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spark Weather/Spark_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spark_WeatherApp.swift
+//  Spark Weather
+//
+//  Created by Bartosz Bogusławski on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Spark_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
